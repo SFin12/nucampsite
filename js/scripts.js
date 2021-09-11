5d8615c6b42fff4c1a5fca7e0381b97d31abeb49
Sean Finegan <sean.j.finegan@gmail.com>
2021-09-11 11:27:28 -0700

    Week4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -11,5 +11,12 @@ $(function(){
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+    $('#reserveButton').click(function () {
+        $('#reserveModal').modal('show')
+    })
+    $('#loginButton').click(function () {
+        console.log("login")
+        $('#loginModal').modal('show')
+    })
   
 })
